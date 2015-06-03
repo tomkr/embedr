0117eafd1c79986717ccab6aad553a5cfd96d72a
Tom Kruijsen <tom@tomkr.org>
2015-06-03 18:42:00 +0200

    Display rudimentary results.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+  $('.js-search').on('input', function(e) {
+    var query = $(e.target).val();
+    var $results = $('#results');
+    $results.html('');
+    $.getJSON('http://localhost:4567', {query: query}, function(data) {
+      data.forEach(function(image) {
+        var imageId = image['_id'];
+        var imageUrl = 'http://iiifhawk.klokantech.com/'+imageId+'/full/100,100/0/native.jpg';
+        $results.html("<img src='"+imageUrl+"'>");
+        console.log($results.html())
+      });
+    });
+  });
+});
