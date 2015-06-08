6b3c89a5268f23a430b1fffccb797ca7a1c98a4f
Tom Kruijsen <tom@tomkr.org>
2015-06-08 21:24:11 +0200

    Add components for results.

--- app/components/components.jsx
+++ app/components/components.jsx
@@ -1,12 +1,19 @@
 var Search = React.createClass({
+  getInitialState: function() {
+    return {results: []};
+  },
   search: function(query) {
+    var self = this;
     $.getJSON('https://hawk-frontend-staging.herokuapp.com?query='+query, function(data) {
-      console.log(data);
+      self.setState({results: data});
     });
   },
   render: function() {
     return (
-      <SearchBar search={this.search}/>
+      <div className="search">
+        <SearchBar search={this.search} />
+        <ResultList results={this.state.results} />
+      </div>
     );
   }
 });
@@ -19,6 +26,31 @@ var SearchBar = React.createClass({
   render: function() {
     return (
       <input className="form-control input-lg" placeholder="Search" onChange={this.handleChange}/>
+    );
+  }
+});
+
+var ResultList = React.createClass({
+  render: function() {
+    console.log(this.props)
+    var resultNodes = this.props.results.map(function (result) {
+      return (
+        <Result key={result._id} id={result._id} />
+      );
+    });
+    return (
+      <div className="resultList">
+        {resultNodes}
+      </div>
+    )
+  }
+});
+
+var Result = React.createClass({
+  render: function() {
+    var source = "http://iiifhawk.klokantech.com/"+this.props.id+"/full/100,100/0/native.jpg";
+    return (
+      <img src={source} />
     );
   }
 });
