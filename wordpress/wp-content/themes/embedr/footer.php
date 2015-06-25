2e50dadd0bfb2631b08ac50583285ffc7f3556d6
Tom Kruijsen <tom@tomkr.org>
2015-06-25 12:13:35 +0200

    Add initial wordpress folder.

---
+++ wordpress/wp-content/themes/embedr/footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "site-content" div and all content after.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?>
+
+<script src=scripts/04a97af3.vendor.js></script>
+<script src=scripts/d29dc0c7.main.js></script>
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wordpress/wp-content/themes/embedr/header.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+ add_filter('show_admin_bar', '__return_false');
+?>
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<![endif]-->
+	
+	<?php //wp_head(); ?>
+	<link rel=stylesheet href="/styles/b0e220be.main.css"> 
+</head>
+
+<body>
