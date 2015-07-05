d081165fefc406bc22898871ea33b1a7eb4a1d57
Tom Kruijsen <tom@tomkr.org>
2015-07-05 10:30:33 +0200

    Split up watch tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function (grunt) {
         tasks: ['browserify:app']
       },
       wordpress: {
-        files: ['app/**/*', 'wordpress/**/*'],
+        files: ['wordpress/**/*'],
         tasks: ['build']
       },
       assets: {
