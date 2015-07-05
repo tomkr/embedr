e0937db5b6c0d0bc497e90f5226c43ebb0f5291e
Tom Kruijsen <tom@tomkr.org>
2015-07-05 17:31:09 +0200

    Updates to viewer.

--- app/scripts/viewer.js
+++ app/scripts/viewer.js
@@ -1,3 +1,13 @@
 var Viewer = require('./components/viewer.jsx')
 
 React.render(<Viewer />, document.getElementById('viewer'));
+
+$(function(){
+  $('#map').on('mouseover', function(e) {
+    $('#viewer').show();
+  });
+  $('#map').on('mouseout', function(e) {
+    if ($(e.toElement).closest('#viewer').length > 0) return;
+    $('#viewer').hide();
+  });
+});
