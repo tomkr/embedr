c2698d571cb9f7fae062a30cfffb3133ab911a48
Tom Kruijsen <tom@tomkr.org>
2015-07-08 18:25:22 +0200

    Wrap embed iframe in div.

--- app/scripts/components/embed_popup.jsx
+++ app/scripts/components/embed_popup.jsx
@@ -4,7 +4,7 @@ var IIIFImage = require('./iiif_image.jsx');
 var EmbedPopup = React.createClass({
   render: function() {
     var embedLink = "http://media.embedr.eu/" + this.props.id;
-    var embedText = "<iframe src=\"" + embedLink + "\"></iframe>"
+    var embedText = '<div class="embdr_wrapper" style="position: relative; height: 400px;"><iframe style="border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="' + embedLink + '">Your browser doesn\'t support iFrames.</iframe></div>'
     return (
       <div className="embed__popup">
         <CloseButton onClick={this.props.close} />
