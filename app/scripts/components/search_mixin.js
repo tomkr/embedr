b8bc055d47a9d39703595d42b275ef3d42e2459c
Tom Kruijsen <tom@tomkr.org>
2015-08-16 16:31:40 +0200

    Use updated search functionality.

--- app/scripts/components/search_mixin.js
+++ app/scripts/components/search_mixin.js
@@ -5,10 +5,11 @@ var executeQuery = function (queryOptions, callback) {
   }
   var query = queryOptions.query;
   var license = queryOptions.license;
+  var start = queryOptions.start;
   var fullQuery = query;
   var licensesQuery = licenses[license].join(' OR ');
   fullQuery = fullQuery + ' AND (' + licensesQuery +')';
-  $.getJSON('http://embedr.eu/search/?query='+encodeURIComponent(fullQuery), function(data) {
+  $.getJSON('http://embedr.eu/search/?query='+encodeURIComponent(fullQuery)+'&start='+start, function(data) {
     callback(data);
   });
 };
@@ -24,10 +25,11 @@ var SearchMixin = {
   setLicense: function(license) {
     this.setState({'license': license})
   },
-  search: function(query) {
+  search: function(query, start) {
+    var start = start || 0;
     this.setState({searchQuery: query})
     var self = this;
-    executeQuery({query: query, license: this.state.license}, function(data) {
+    executeQuery({query: query, license: this.state.license, start: start}, function(data) {
       self.setState({results: data.hits});
     });
   }
