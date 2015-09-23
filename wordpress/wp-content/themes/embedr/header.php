ebf0f5365690924c049bffc0f52d19b3c932952c
Maarten Zeinstra <mzeinstra@MacBook-Air-Maarten.local>
2015-09-23 13:44:34 +0200

    Added social media Twitter and Facebook images when embedding embedr.eu itself and not image pages.

--- wordpress/wp-content/themes/embedr/header.php
+++ wordpress/wp-content/themes/embedr/header.php
@@ -23,14 +23,15 @@
 
 	<!--Twitter integration-->
 	<meta name="twitter:card" content="summary" />
-  <meta name="twitter:site" content="@embedr_eu" />
-  <meta name="twitter:title" content="Embedr.eu" />
-  <meta name="twitter:description" content="Embed high quality cultural heritage images" />
+	<meta name="twitter:site" content="@embedr_eu" />
+	<meta name="twitter:title" content="Embedr.eu" />
+	<meta name="twitter:description" content="Embed high quality cultural heritage images" />
+	<meta name="twitter:image" content="/images/embedr_social_media_logo.png" />
 
 	<!-- Open Graph for generic site -->
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content="Embedr.eu | Image Embedding Done Right"/>
-	<meta property="og:image" content=""/>
+	<meta property="og:image" content="/images/embedr_social_media_logo.png"/>
 	<meta property="og:site_name" content="Embedr.eu"/>
 	<meta property="og:description" content="Embedr.eu is an image embedding platform that allows easy embedding of reusable cultural heritage images."/>
 
