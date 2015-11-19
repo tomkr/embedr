4b7a7eabf633a0f3ec276b7fc3b28dedfddf19b2
Tom Kruijsen <tom@tomkruijsen.nl>
2015-11-19 07:35:51 +0100

    Add tests for full viewer.

--- test/components/viewer_spec.jsx
+++ test/components/viewer_spec.jsx
@@ -27,7 +27,27 @@ describe("Viewer", function () {
     it("shows the zoom buttons", function() {
       expect(this.toolbar.props.children[1]).to.not.equal(null);
     });
-  })
+  });
+
+  describe("Full viewer", function() {
+    before(function() {
+      shallowRenderer.render(<Viewer id="1" type="full"/>);
+      this.result = shallowRenderer.getRenderOutput();
+      this.toolbar = this.result.props.children[0];
+    });
+
+    it("renders an div tag as its root element", function () {
+      expect(this.result.type).to.equal("div");
+    });
+
+    it("shows zoom buttons", function() {
+      expect(this.toolbar.props.children[1]).to.not.equal(null);
+    });
+
+    it("shows the region button", function() {
+      expect(this.toolbar.props.children[2]).to.not.equal(null);
+    });
+  });
 
   describe("No zoom viewer", function() {
     before(function() {
